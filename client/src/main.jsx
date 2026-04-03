72d315693f9bfe3db06760b324ca9b01f1f0cf38
Halenur Gurel <halenur.gurel@hotmail.com>
2026-04-03 18:35:04 +0300

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import 'modern-normalize';
 import './index.css';
 import App from './App.jsx';
 import { store } from './redux/store';
 import { injectStore } from './api/axiosInstance';
-
 injectStore(store);
 
 createRoot(document.getElementById('root')).render(
